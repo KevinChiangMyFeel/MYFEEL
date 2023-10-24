eb2e20cb30cd3bea1335944669c67da4df97d3aa
Kevin <kevin.chiang@myfeel-tw.com>
2023-10-25 01:23:08 +0800

    10/25

---
+++ _nuxt/dbbc2b6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{452:function(n,t,e){"use strict";e.r(t);var r=e(70),component=Object(r.a)({},(function(){this._self._c;return this._m(0)}),[function(){var n=this._self._c;return n("div",[n("h2",[this._v("Crowdfunding Index")])])}],!1,null,null,null);t.default=component.exports}}]);
